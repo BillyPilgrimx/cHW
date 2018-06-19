8dd3d5a0f8f5118d2c3b9f71c2f9c995a4922f19
BillyPilgrimx <vadko87@gmail.com>
2018-06-19 22:12:42 +0300

    first complition of the WriteBitmap, AddHouse, AddRoad, AddCrossroad functions, the colors are constants in the code and omitted in the input file

--- HW4_VadimKobrinsky/HW4_VadimKobrinsky/HW4_VadimKobrinsky.c
+++ HW4_VadimKobrinsky/HW4_VadimKobrinsky/HW4_VadimKobrinsky.c
@@ -8,7 +8,7 @@
 // size definitions
 #define HEIGHT 800
 #define WIDTH 1000
-#define R_WIDTH 20
+#define HR_WIDTH 20  // half of road width
 #define CR_RADIUS 15
 
 // files definitions
@@ -30,8 +30,8 @@
 #define COM_HOUSE_B 204
 // industrial house - Royal (yellow)
 #define IND_HOUSE_R 250
-#define IND_HOUSE_R 218
-#define IND_HOUSE_R 94
+#define IND_HOUSE_G 218
+#define IND_HOUSE_B 94
 //  road - Light Grey
 #define ROAD_R 202
 #define ROAD_G 204
@@ -50,7 +50,7 @@ typedef struct color
 typedef struct house
 {
 	int cx, cy;			// central x & y coordinates
-	int height, width;	
+	int height, width;
 	int houseType;		// 1 = residential, 2 = commercial, 3 = industrial
 	COLOR color;
 
@@ -92,7 +92,10 @@ typedef struct crossroad_node
 } CROSSROAD_NODE;
 
 // prototypes
-
+void WriteBitmap(char* out_fname, HOUSE_NODE* houses, ROAD_NODE* roads, CROSSROAD_NODE* crossroads);
+void AddHouse(unsigned char* bmp, HOUSE_NODE* houses);
+void AddRoad(unsigned char* bmp, ROAD_NODE* roads);
+void AddCrossroad(unsigned char* bmp, CROSSROAD_NODE* crossroads);
 
 // main
 void main()
@@ -100,9 +103,6 @@ void main()
 	HOUSE_NODE* houses = NULL;		   // dynamic array of houses
 	ROAD_NODE* roads = NULL;		   // dynamic array of roads
 	CROSSROAD_NODE* crossroads = NULL; // dynamic array of crossroads
-	int num_houses;
-	int num_roads;
-	int num_crossroads;
 
 	FILE* pf = fopen(IN_FNAME, "r");
 	if (pf == NULL) // in case the file cannot be found
@@ -111,25 +111,25 @@ void main()
 		exit(1);	// emergency exit
 	}
 
-	houses = ReadHouses(pf, num_houses);
-	roads = ReadRoads(pf, num_roads);
-	crossroads = CreateCrossRoads(roads, num_roads, num_crossroads);
+	houses = ReadHouses(pf, houses);
+	roads = ReadRoads(pf, roads);
+	crossroads = CreateCrossRoads(roads, crossroads);
 
 
 
 	fclose(pf);
-	WriteBitmap(OUT_FNAME, houses, num_houses, roads, num_roads, crossroads, num_crossroads);
+	WriteBitmap(OUT_FNAME, houses, roads, crossroads);
 	printf("Done!\n");
 }
 
-void WriteBitmap(char* out_fname, HOUSE_NODE* houses, int num_houses, ROAD_NODE* roads, int num_roads, CROSSROAD_NODE* crossroads, int num_crossroads)
+void WriteBitmap(char* out_fname, HOUSE_NODE* houses, ROAD_NODE* roads, CROSSROAD_NODE* crossroads)
 {
 	FILE* pf = fopen(out_fname, "wb");
 	BITMAPFILEHEADER bf;
 	BITMAPINFOHEADER bi;
 	unsigned char* bmp;
 	int i, j, size;
-	
+
 	size = HEIGHT * WIDTH * 3;
 	bmp = (unsigned char*)malloc(size * sizeof(unsigned char));
 	// defining file header
@@ -154,29 +154,172 @@ void WriteBitmap(char* out_fname, HOUSE_NODE* houses, int num_houses, ROAD_NODE*
 	}
 
 	// adding houses, roads and crossroads
-	for (i = 0; i < num_houses; i++)
-	{
-		AddHouse(bmp, houses[i]);
-	}
+	AddHouse(bmp, houses);
+	AddRoad(bmp, roads);
+	AddCrossroad(bmp, crossroads);
 
-	for (i = 0; i < num_roads; i++)
-	{
-		AddRoad(bmp, roads[i]);
-	}
-
-	for (i = 0; i < num_crossroads; i++)
-	{
-		AddCrossroad(bmp, crossroads[i]);
-	}
 
 	// saving the data
 	fwrite(&bf, sizeof(bf), 1, pf);
 	fwrite(&bi, sizeof(bf), 1, pf);
 	fwrite(bmp, 1, size, pf);
-	
+
 	fclose(pf);
 	free(bmp);
 }
+
+void AddHouse(unsigned char* bmp, HOUSE_NODE* houses)
+{
+	int i, j, left, right, bottom, top;
+
+	if (houses == NULL) // in case there are no houses to add
+	{
+		printf("There are no houses to create...\n");
+		return;
+	}
+	else
+	{
+		left = (houses->aHouse.cx) - (houses->aHouse.width / 2);
+		right = (houses->aHouse.cx) + (houses->aHouse.width / 2);
+		bottom = (houses->aHouse.cy) - (houses->aHouse.height / 2);
+		top = (houses->aHouse.cy) + (houses->aHouse.height / 2);
+
+		for (i = bottom; i < top; i++)
+		{
+			for (j = left; j < right; j++)
+			{
+				switch (houses->aHouse.houseType)
+				{
+				case 1: // residential house
+					bmp[(i * WIDTH + j) * 3 + 0] = RES_HOUSE_R; // red
+					bmp[(i * WIDTH + j) * 3 + 1] = RES_HOUSE_G; // green
+					bmp[(i * WIDTH + j) * 3 + 2] = RES_HOUSE_B; // blue
+					break;
+				case 2: // commercial house
+					bmp[(i * WIDTH + j) * 3 + 0] = COM_HOUSE_R; // red
+					bmp[(i * WIDTH + j) * 3 + 1] = COM_HOUSE_G; // green
+					bmp[(i * WIDTH + j) * 3 + 2] = COM_HOUSE_B; // blue
+					break;
+				case 3: // industrail house
+					bmp[(i * WIDTH + j) * 3 + 0] = IND_HOUSE_R; // red
+					bmp[(i * WIDTH + j) * 3 + 1] = IND_HOUSE_G; // green
+					bmp[(i * WIDTH + j) * 3 + 2] = IND_HOUSE_B; // blue
+					break;
+				}
+			}
+		}
+
+		if (houses->next != NULL)
+		{
+			AddHouse(bmp, houses->next);
+		}
+	}
+}
+
+void AddRoad(unsigned char* bmp, ROAD_NODE* roads)
+{
+	int i, j, left, right, bottom, top;
+
+	if (roads == NULL) // in case there are no roads to add
+	{
+		printf("There are no roads to create...\n");
+		return;
+	}
+	else
+	{
+		if (roads->aRoad.y1 == roads->aRoad.y2) // in case a horizontal road
+		{
+			left = roads->aRoad.x1;
+			right = roads->aRoad.x2;
+			bottom = (roads->aRoad.y1 - HR_WIDTH);
+			top = (roads->aRoad.y1 + HR_WIDTH);
+		}
+		else									// in case a vertical road
+		{
+			left = (roads->aRoad.x1 - HR_WIDTH);
+			right = (roads->aRoad.x1 + HR_WIDTH);
+			bottom = roads->aRoad.y1;
+			top = roads->aRoad.y2;
+		}
+
+		for (i = bottom; i < top; i++)
+		{
+			for (j = left; j < right; j++)
+			{
+				bmp[(i * WIDTH + j) * 3 + 0] = ROAD_R; // red
+				bmp[(i * WIDTH + j) * 3 + 1] = ROAD_G; // green
+				bmp[(i * WIDTH + j) * 3 + 2] = ROAD_B; // blue
+			}
+		}
+
+		if (roads->next != NULL)
+		{
+			AddHouse(bmp, roads->next);
+		}
+	}
+}
+
+void AddCrossroad(unsigned char* bmp, CROSSROAD_NODE* crossroads)
+{
+	int i, j, left, right, bottom, top, cx, cy;
+	double dist;
+
+	if (crossroads == NULL) // in case there are no crossroads to add
+	{
+		printf("There are no crossroads to create...\n");
+		return;
+	}
+	else
+	{
+		if (crossroads->aCrossRoad.roadA.y1 == crossroads->aCrossRoad.roadA.y2) // in case roadA is horizontal (and roadB vertical)
+		{
+			cx = crossroads->aCrossRoad.roadB.x1;
+			cy = crossroads->aCrossRoad.roadA.y1;
+		}
+		else                                                                    // in case roadA is vertical (and roadB horizontal)
+		{
+			cx = crossroads->aCrossRoad.roadA.x1;
+			cy = crossroads->aCrossRoad.roadB.y1;
+		}
+
+		left = cx - CR_RADIUS;
+		right = cx + CR_RADIUS;
+		bottom = cy - CR_RADIUS;
+		top = cy + CR_RADIUS;
+
+		for (i = bottom; i < top; i++)
+		{
+			for (j = left; j < right; j++)
+			{
+				dist = sqrt((i - cy) * (i - cy) + (j - cx) * (j - cx));
+				if (dist < CR_RADIUS)
+				{
+					bmp[(i * WIDTH + j) * 3 + 0] = CROSSROAD_R; // red
+					bmp[(i * WIDTH + j) * 3 + 1] = CROSSROAD_G; // green
+					bmp[(i * WIDTH + j) * 3 + 2] = CROSSROAD_B; // blue
+				}
+			}
+		}
+
+		if (crossroads->next != NULL)
+		{
+			AddHouse(bmp, crossroads->next);
+		}
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
