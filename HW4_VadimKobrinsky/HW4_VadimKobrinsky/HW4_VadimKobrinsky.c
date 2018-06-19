7cb6ea69564e0e6101cbe1b373dfdf1a1d9598fd
BillyPilgrimx <vadko87@gmail.com>
2018-06-19 19:57:54 +0300

    work on the skeleton/main

--- HW4_VadimKobrinsky/HW4_VadimKobrinsky/HW4_VadimKobrinsky.c
+++ HW4_VadimKobrinsky/HW4_VadimKobrinsky/HW4_VadimKobrinsky.c
@@ -5,32 +5,70 @@
 #include <string.h>
 #include <Windows.h>
 
-// world definition
+// constants definitions
 #define HEIGHT 800
 #define WIDTH 1000
+#define R_WIDTH 20
+#define IN_FNAME "input.txt"
+#define OUT_FNAME "output.txt"
 
 // type definitions
 typedef struct color
 {
 	int red, green, blue;
-
 } COLOR;
 
 typedef struct house
 {
-	int cx, cy, height, width;
+	int cx, cy;
+	int height, width;
 	char houseType;
 	COLOR color;
 
 } HOUSE;
 
+typedef struct road
+{
+	int x1, y1, x2, y2;
+
+} ROAD;
+
+typedef struct house_node
+{
+	HOUSE aHouse;
+	struct house_node* next;
+
+} HOUSE_NODE;
+
+typedef struct road_node
+{
+	ROAD aRoad;
+	struct road_node* next;
+} ROAD_NODE;
+
 // prototypes
-
-
 
 
 // main
 void main()
 {
+	HOUSE* houseArr; // dynamic array of houses
+	ROAD* roadArr; // dynamic array of roads
+	int num_houses;
+	int num_roads;
+	FILE* pf = fopen(IN_FNAME, "r");
 
+	if (pf == NULL)
+	{
+		printf("Can't open the file %s...\n", IN_FNAME);
+		exit(1); // emergency exit
+	}
+
+	houseArr = ReadHouses(pf, num_houses);
+	roadArr = ReadRoads(pf, num_roads);
+
+
+
+	fclose(pf);
+	WriteBitmap(OUT_FNAME, )
 }
