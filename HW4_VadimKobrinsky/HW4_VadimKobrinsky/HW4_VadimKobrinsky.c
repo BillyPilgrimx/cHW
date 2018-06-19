03640d98f6b9beb883a37d6b59c968b62d11a48b
BillyPilgrimx <vadko87@gmail.com>
2018-06-19 18:59:45 +0300

    first commit on 4th exercise

---
+++ HW4_VadimKobrinsky/HW4_VadimKobrinsky/HW4_VadimKobrinsky.c
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <Windows.h>
+
+// world definition
+#define HEIGHT 800
+#define WIDTH 1000
+
+// type definitions
+typedef struct color
+{
+	int red, green, blue;
+
+} COLOR;
+
+typedef struct house
+{
+	int cx, cy, height, width;
+	char houseType;
+	COLOR color;
+
+} HOUSE;
+
+// prototypes
+
+
+
+
+// main
+void main()
+{
+
+}
